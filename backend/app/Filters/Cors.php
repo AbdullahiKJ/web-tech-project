733b1c0060af028a02413a0be6a17daa5fd91889
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2026-02-24 14:37:06 +0000

    feat: convert the project to a codeigniter backend and next js frontend setup

---
+++ backend/app/Filters/Cors.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class Cors implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        header('Access-Control-Allow-Origin: http://localhost:5173');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null) {}
+}
