6ab098ee9ba089335d0a318568ef7ccbbaa0981f
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2026-03-08 23:17:45 +0000

    feat: create review api

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -7,7 +7,19 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/api/test', 'Api::test');
+
+// User Routes
 $routes->post('/users', 'Users::create');
+
+// Review Routes
+$routes->post('/reviews', 'Reviews::create');
+$routes->get('/reviews', 'Reviews::index');
+$routes->get('/reviews/(:segment)', 'Reviews::showReview/$1/1');
+$routes->get('/account/(:segment)/reviews', 'Reviews::showReview/$1/2');
+$routes->get('/media/(:segment)/reviews', 'Reviews::showReview/$1/3');
+$routes->delete('/reviews/(:segment)', 'Reviews::delete/$1');
+$routes->put('/reviews/(:segment)', 'Reviews::update/$1');
+
 $routes->options('(:any)', static function () {
     return response()->setStatusCode(200);
 });
---
+++ backend/app/Models/ReviewModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ReviewModel extends Model
+{
+    protected $table = 'reviews';
+    protected $allowedFields = ['id', 'movie_id', 'rating', 'review_description', 'user_id'];
+
+    public function getReviews($slug = false, $type = 1)
+    {
+        // Get all reviews
+        if ($slug == false)
+            return $this->findAll();
+        else {
+            // Get the review with the given id
+            if ($type == 1) {
+                return $this->where('id', $slug)->findAll();
+            }
+            // Get all reviews made by the given account
+            else if ($type == 2) {
+                return $this->where('user_id', $slug)->findAll();
+            }
+            // Get all reviews for the given movie/show
+            else
+                return $this->where('movie_id', $slug)->findAll();
+        }
+    }
+}
