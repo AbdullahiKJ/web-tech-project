fb223f192af172b66afbc1de92fd006811726fd2
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2026-03-09 00:19:31 +0000

    feat: create watchlist api

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -15,10 +15,15 @@ $routes->post('/users', 'Users::create');
 $routes->post('/reviews', 'Reviews::create');
 $routes->get('/reviews', 'Reviews::index');
 $routes->get('/reviews/(:segment)', 'Reviews::showReview/$1/1');
-$routes->get('/account/(:segment)/reviews', 'Reviews::showReview/$1/2');
+$routes->get('/users/(:segment)/reviews', 'Reviews::showReview/$1/2');
 $routes->get('/media/(:segment)/reviews', 'Reviews::showReview/$1/3');
 $routes->delete('/reviews/(:segment)', 'Reviews::delete/$1');
 $routes->put('/reviews/(:segment)', 'Reviews::update/$1');
+
+// Watchlist Routes
+$routes->post('/watchlist/(:segment)/(:segment)', 'Watchlists::add/$1/$2');
+$routes->delete('/watchlist/(:segment)/(:segment)', 'Watchlists::remove/$1/$2');
+$routes->get('/watchlist/(:segment)', 'Watchlists::show/$1');
 
 $routes->options('(:any)', static function () {
     return response()->setStatusCode(200);
