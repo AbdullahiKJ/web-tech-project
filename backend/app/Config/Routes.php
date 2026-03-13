343eb1bbb88bcd1ce7530f28012767295da93f02
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2026-03-13 15:45:04 +0000

    chore: rename user review and movie review route

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -14,9 +14,9 @@ $routes->post('/users', 'Users::create');
 // Review Routes
 $routes->post('/reviews', 'Reviews::create');
 $routes->get('/reviews', 'Reviews::index');
+$routes->get('/reviews/users/(:segment)', 'Reviews::showReview/$1/2');
+$routes->get('/reviews/movie/(:segment)', 'Reviews::showReview/$1/3');
 $routes->get('/reviews/(:segment)', 'Reviews::showReview/$1/1');
-$routes->get('/users/(:segment)/reviews', 'Reviews::showReview/$1/2');
-$routes->get('/media/(:segment)/reviews', 'Reviews::showReview/$1/3');
 $routes->delete('/reviews/(:segment)', 'Reviews::delete/$1');
 $routes->put('/reviews/(:segment)', 'Reviews::update/$1');
 
