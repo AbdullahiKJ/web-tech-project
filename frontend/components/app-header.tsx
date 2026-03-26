dd8f87f0245f18518596fc7dbf569dbf9c556ea0
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2026-03-26 23:11:27 +0000

    style: add daisy theme controller

--- frontend/components/app-header.tsx
+++ frontend/components/app-header.tsx
@@ -1,12 +1,13 @@
+'use client'
+
 import {
-    BookOpen,
-    Folder,
     Search,
     CircleUser,
     Clock,
     Star,
 } from 'lucide-react';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 type NavItem = {
     title?: string;
@@ -32,65 +33,58 @@ const mainNavItems: NavItem[] = [
     },
 ];
 
-const rightNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        icon: BookOpen,
-    },
-];
-
-const activeItemStyles =
-    'text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100';
-
 export function AppHeader() {
+    const [theme, setTheme] = useState<"light"|"dark">("light");
+
+    useEffect(() => {
+    // Check if the theme is stored in local storage
+    const saved = localStorage.getItem("theme") as "light"|"dark"|null;
+    if (saved) {
+        setTheme(saved);
+    } 
+    else {
+        // Check if the system theme is dark if there is no local storage theme
+        const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        setTheme(prefersDark ? "dark" : "light");
+    }
+    }, []);
+
+    // Update the theme when the theme state is updated
+    useEffect(() => {
+    document.documentElement.dataset.theme = theme;
+    localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    // Update the theme and store 
+    function toggleTheme() {
+        setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+    }
+  
     return (
         <>
             <div className="border-sidebar-border/80 bg-gray-950">
                 <div className="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
                     {/* Desktop Navigation */}
                     <div className="ml-6 hidden h-full items-center space-x-6 lg:flex">
-                                {mainNavItems.map((item, index) => (
-                                        <Link
-                                            href={item.href || "#"}
-                                            className='h-9 cursor-pointer px-3 flex flex-row'
-                                            key={index}
-                                        >
-                                            {item.icon && (
-                                                <item.icon className="mr-2 h-4 w-4" />
-                                            )}
-                                            {item.title}
-                                        </Link>
-                                ))}
+                        {mainNavItems.map((item, index) => (
+                                <Link
+                                    href={item.href || "#"}
+                                    className='h-9 cursor-pointer px-3 flex flex-row'
+                                    key={index}
+                                >
+                                    {item.icon && (
+                                        <item.icon className="mr-2 h-4 w-4" />
+                                    )}
+                                    {item.title}
+                                </Link>
+                        ))}
                     </div>
 
                     <div className="ml-auto flex items-center space-x-2">
                         <div className="relative flex items-center space-x-1">
-                            <button
-                                className="group h-9 w-9 cursor-pointer"
-                            >
+                            <button className="group h-9 w-9 cursor-pointer">
                                 <Search className="size-5! opacity-80 group-hover:opacity-100" />
                             </button>
-                            <div className="ml-1 hidden gap-1 lg:flex">
-                                {rightNavItems.map((item, index) => (
-                                                <a
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                    className="group inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium text-accent-foreground ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
-                                                    key={index}
-                                                >
-                                                    <span className="sr-only">
-                                                        {item.title}
-                                                    </span>
-                                                    {item.icon && (
-                                                        <item.icon className="size-5 opacity-80 group-hover:opacity-100" />
-                                                    )}
-                                                </a>
-                                ))}
-                            </div>
                         </div>
                         <Link
                             href="/sign-in"
@@ -99,7 +93,12 @@ export function AppHeader() {
                             Log in
                         </Link>
                         <label className="toggle text-base-content">
-                            <input type="checkbox" value="synthwave" className="theme-controller" />
+                            <input
+                                type="checkbox"
+                                checked={theme === 'dark'}
+                                onChange={toggleTheme}
+                                className="theme-controller"
+                            />
                             <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
                             <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
                         </label>
